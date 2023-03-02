be25d7a47672f0e5b78dc13b00fa793e3823b9cc
Diana Zawislak <diana.zawislak@icloud.com>
2023-03-02 00:13:03 -0500

    check for winning move

--- main.py
+++ main.py
@@ -22,9 +22,50 @@ def _board_is_full():
     return True
 
 
+def is_winning_move(player):
+    if player == 1:
+        announcement = 'Player 1 Won'
+    else:
+        announcement = 'Player 2 Won'
+    for r in range(ROWS):
+        if board[r][0] == player and board[r][1] == player and board[r][2] == player:
+            print(announcement)
+        return True
+    for c in range(COLUMNS):
+        if board[0][c] == player and board[1][c] == player and board[2][0] == player:
+            print(announcement)
+        return True
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        print(announcement)
+        return True
+    if board[2][0] == player and board[1][1] == player and board[0][2] == player:
+        print(announcement)
+        return True
+
 board = np.zeros((ROWS, COLUMNS))
 
-print(board)
-mark(1, 0, 2)
-print(board)
-print(is_valid_mark(1, 0))
+game_over = False
+
+Turn = 0
+
+while not game_over:
+    if Turn % 2 == 0:
+        # Player 1
+        row = int(input('Player 1: choose row number (0-2):'))
+        col = int(input('Player 1: choose column number (0-2):'))
+        if is_valid_mark(row, col):
+            mark(row, col, 1)
+        else:
+            Turn -= 1
+
+        # Player 2
+        row = int(input('Player 2: choose row number (0-2):'))
+        col = int(input('Player 2: choose column number (0-2):'))
+        if is_valid_mark(row, col):
+            mark(row, col, 2)
+        else:
+            Turn -= 1
+
+    Turn += 1
+
+    print(board)
